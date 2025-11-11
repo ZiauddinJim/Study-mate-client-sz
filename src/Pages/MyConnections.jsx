19abefce0336d90c87e64c2c2db26f646bbc31fa
SheikhMdZiauddin <ziauddinjim@outlook.com>
2025-11-11 10:17:03 +0600

    Complete myConnection design

--- src/Pages/MyConnections.jsx
+++ src/Pages/MyConnections.jsx
@@ -1,9 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Container from '../MyComponents/Container';
+import { useEffect } from 'react';
+import useAxios from '../Hooks/useAxios';
+import useAuth from '../Hooks/useAuth';
 
 const MyConnections = () => {
+    const Axios = useAxios()
+    const { user } = useAuth()
+    const [data, setData] = useState([])
+    // console.log(user.email);
+    useEffect(() => {
+        Axios.get(`/my-connection?email=${user.email}`,)
+            .then(data => {
+                console.log(data.data);
+                setData(data.data)
+            })
+    }, [Axios, user])
+    
+
     return (
-        <div>
-            My Connection
+        <div className='my-10'>
+            <Container>
+                <div className="overflow-x-auto">
+                    <table className="table">
+                        {/* head */}
+                        <thead>
+                            <tr className='text-primary'>
+                                <th>Sl.</th>
+                                <th>Partner Details</th>
+                                <th>Subject</th>
+                                <th>Study Mode</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                data.map((data, i) => {
+                                    const { _id, name, ProfileImage, subject, email, studyMode } = data
+                                    return (
+                                        <tr key={_id}>
+                                            <th className='text-primary'>{i + 1}</th>
+                                            <td>
+                                                <div className="flex items-center gap-3">
+                                                    <div className="avatar">
+                                                        <div className="mask mask-squircle h-12 w-12">
+                                                            <img
+                                                                src={ProfileImage}
+                                                                alt={name} />
+                                                        </div>
+                                                    </div>
+                                                    <div>
+                                                        <div className="font-bold">{name}</div>
+                                                        <div className="text-sm opacity-50">{email}</div>
+                                                    </div>
+                                                </div>
+                                            </td>
+                                            <td>
+                                                {subject}
+                                                {/* <br />
+                                                <span className="badge badge-ghost badge-sm">{studyMode}</span> */}
+                                            </td>
+                                            <td>{studyMode}</td>
+                                            <th className='flex gap-2'>
+                                                <button className="btn btn-outline hover:btn-primary hover:text-white btn-xs">Updata</button>
+                                                <button className="btn btn-outline hover:btn-secondary hover:text-white btn-xs">Delete</button>
+                                            </th>
+                                        </tr>
+                                    )
+                                })
+                            }
+
+                        </tbody>
+                    </table>
+                </div>
+            </Container>
         </div>
     );
 };
--- src/Pages/PartnerDetails.jsx
+++ src/Pages/PartnerDetails.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { FaStar, FaMapMarkerAlt, FaUserFriends } from "react-icons/fa";
-import { MdOutlineSchool, MdAccessTime, MdOutlineWorkOutline } from "react-icons/md";
+import { MdOutlineSchool, MdAccessTime, MdOutlineWorkOutline, MdOnlinePrediction } from "react-icons/md";
 import useAxios from "../Hooks/useAxios";
 import { useParams } from "react-router";
+import Swal from "sweetalert2";
+import useAuth from "../Hooks/useAuth";
 
 const PartnerDetails = () => {
     const { id } = useParams()
     const Axios = useAxios()
+    const { user } = useAuth()
+    const [refetch, setRefetch] = useState(false)
+    // console.log(user.displayName);
     const [partner, setPartner] = useState([])
 
     useEffect(() => {
@@ -15,24 +20,66 @@ const PartnerDetails = () => {
                 // console.log(data.data);
                 setPartner(data.data)
             })
-    }, [Axios, id])
+    }, [Axios, id, refetch])
+
+    const { ProfileImage, name, email,
+        rating, subject, availabilityTime,
+        experienceLevel, location,
+        partnerCount, studyMode, _id } = partner
+
+    const handleConnection = (e) => {
+        e.preventDefault();
+
+        Axios.post("/connection", {
+            ProfileImage, name, email,
+            rating, subject, availabilityTime,
+            experienceLevel, location,
+            partnerCount, studyMode, connectionBy: user.email
+        })
+            .then(data => {
+                console.log(data.data);
+                if (data.data.insertedId) {
+                    Swal.fire({
+                        position: "center",
+                        icon: "success",
+                        title: "Your Connection has been created",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            }).catch(error => {
+                console.error("Connection error:", error);
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: error.response?.data?.message || "Something went wrong! Please try again.",
+                });
+            })
+
+        Axios.patch(`/partner-count/${_id}`)
+            .then(data => {
+                console.log(data.data);
+                setRefetch(!refetch)
+            })
+    }
 
     return (
-        <div className="min-h-screen partner-details flex justify-center items-center p-4">
+        <div className="min-h-[60vh] my-10 partner-details flex justify-center items-center p-4">
             <div className="max-w-3xl w-full bg-base-100 shadow-xl rounded-2xl overflow-hidden">
                 <div className="flex flex-col md:flex-row">
                     {/* Left Section: Image */}
                     <div className="md:w-1/3 flex justify-center items-center bg-gradient p-6">
                         <img
-                            src={partner.ProfileImage}
-                            alt={partner.name}
+                            src={ProfileImage}
+                            alt={name}
                             className="w-40 h-40 object-cover rounded-full border-4 border-white"
                         />
                     </div>
 
                     {/* Right Section: Info */}
                     <div className="md:w-2/3 p-6">
-                        <h2 className="text-2xl font-bold mb-2">{partner.name}</h2>
+                        <h2 className="text-2xl font-bold">{name}</h2>
+                        <p className=" mb-3">{email}</p>
 
                         {/* Rating */}
                         <div className='flex items-center mb-3'>
@@ -40,7 +87,7 @@ const PartnerDetails = () => {
                                 const starValue = index + 1;
                                 return (
                                     <FaStar key={index} size={20} className="mr-1"
-                                        color={starValue <= partner.rating ? "#2663EB" : "#E4E5E9"} />
+                                        color={starValue <= rating ? "#2663EB" : "#E4E5E9"} />
                                 );
                             })}
                         </div>
@@ -49,41 +96,48 @@ const PartnerDetails = () => {
                             <div className="flex items-center gap-2">
                                 <MdOutlineSchool className="text-primary" />
                                 <p>
-                                    <span className="font-semibold">Subject:</span> {partner.subject}
+                                    <span className="font-semibold">Subject:</span> {subject}
                                 </p>
                             </div>
                             <div className="flex items-center gap-2">
                                 <MdAccessTime className="text-primary" />
                                 <p>
                                     <span className="font-semibold">Availability:</span>{" "}
-                                    {partner.availabilityTime}
+                                    {availabilityTime}
                                 </p>
                             </div>
                             <div className="flex items-center gap-2">
                                 <MdOutlineWorkOutline className="text-primary" />
                                 <p>
                                     <span className="font-semibold">Experience:</span>{" "}
-                                    {partner.experienceLevel}
+                                    {experienceLevel}
                                 </p>
                             </div>
                             <div className="flex items-center gap-2">
                                 <FaMapMarkerAlt className="text-primary" />
                                 <p>
-                                    <span className="font-semibold">Location:</span> {partner.location}
+                                    <span className="font-semibold">Location:</span> {location}
+                                </p>
+                            </div>
+                            <div className="flex items-center gap-2">
+                                <MdOnlinePrediction className="text-primary" />
+                                <p>
+                                    <span className="font-semibold">Study Mode:</span>{" "}
+                                    {studyMode}
                                 </p>
                             </div>
                             <div className="flex items-center gap-2">
                                 <FaUserFriends className="text-primary" />
                                 <p>
                                     <span className="font-semibold">Partner Count:</span>{" "}
-                                    {partner.partnerCount}
+                                    {partnerCount}
                                 </p>
                             </div>
                         </div>
 
                         {/* Button */}
                         <div className="mt-6">
-                            <button className="w-full my-btn font-semibold">
+                            <button onClick={handleConnection} className="w-full my-btn font-semibold">
                                 Send Partner Request
                             </button>
                         </div>
